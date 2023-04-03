f3c6ee5fc30b35bd2cba460b033101da4bc2ab6d
Simon Kunert <skunert@1-A-2.42heilbronn.de>
2023-04-03 18:36:19 +0200

    put character add

--- ft_print_out.c
+++ ft_print_out.c
@@ -1,14 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_print_out.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/03 11:34:29 by skunert           #+#    #+#             */
-/*   Updated: 2023/04/03 18:34:13 by skunert          ###   ########.fr       */
+/*   Created: 2023/04/03 18:29:25 by skunert           #+#    #+#             */
+/*   Updated: 2023/04/03 18:34:36 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <unistd.h>
+#include "ft_printf.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		ft_putchar(s[i]);
+		i++;
+	}
+}
+
+void	ft_putpercent()
+{
+	write(1, "%%", 1);
+}
