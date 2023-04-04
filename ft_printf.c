801caa8d187daadd619e1a51e4024d42130ed9ed
Simon Kunert <skunert@1-A-8.42heilbronn.de>
2023-04-04 13:11:33 +0200

    specifier list func added

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 11:31:07 by skunert           #+#    #+#             */
-/*   Updated: 2023/04/03 13:15:02 by skunert          ###   ########.fr       */
+/*   Updated: 2023/04/04 12:51:32 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,65 +14,11 @@
 #include <string.h>
 #include <stdio.h>
 
-int	check_str(const char *s)
-{
-	int	i;
-	int	count;
-
-	i = 0;
-	count = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == '%')
-		{
-			if (s[i + 1] != '\0' && strchr("cspdiuxX%%", s[i + 1]) != 0)
-				count++;
-		}
-		i++;
-	}
-	return (count);
-}
-
-char	input_type(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == '%')
-		{
-			if (s[i + 1] != '\0' && strchr("cspdiuxX%%", s[i + 1]) != 0)
-				return (s[i + 1]);
-		}
-		i++;
-	}
-	return ('0');
-}
-
-char	*next_arg(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == '%')
-		{
-			if (s[i + 1] != '\0' && strchr("cspdiuxX%%", s[i + 1]) != 0
-				&& s[i + 2] != '\0')
-				return ((char *)s + (i + 2));
-		}
-		i++;
-	}
-	return (NULL);
-}
-
 int ft_printf(const char *s, ...)
 {
-	int	amount_args;
-	int	i;
-	int	x;
+	char	*buff;
+	int		amount_args;
+	int		i;
 	va_list ptr;
 
 	amount_args = check_str(s);
@@ -82,16 +28,9 @@ int ft_printf(const char *s, ...)
 	va_start(ptr, s);
 	while (i < amount_args)
 	{
-		if (input_type(s) == 'd')
-			x = va_arg(ptr, int);
+		read_args(va_arg(ptr, int));
 		i++;
 	}
 	va_end(ptr);
 	return (strlen(s));
-}
-
-int main(void)
-{
-	printf("%d\n", ft_printf("Hasl%dlo", 6));
-	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,9 +6,25 @@
 /*   By: skunert <skunert@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 11:34:29 by skunert           #+#    #+#             */
-/*   Updated: 2023/04/03 18:34:13 by skunert          ###   ########.fr       */
+/*   Updated: 2023/04/04 12:55:54 by skunert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int		check_str(const char *s);
+char	input_type(const char *s);
+char	*next_arg(const char *s);
+char 	*specifier_list(char *s);
+void	ft_putchar(char c);
+void	ft_putstr(char *s);
+void	ft_putpercent();
+
+#endif
